bb2034438946f4f9f24b0a36d5c977e8f21b31d4
Dmitry Kovalev <dkovalev@google.com>
2020-01-23 15:34:50 -0800

    Internal Change
    PiperOrigin-RevId: 291259239

--- python/examples/classification/classify_image.py
+++ python/examples/classification/classify_image.py
@@ -34,9 +34,13 @@ from PIL import Image
 
 import classify
 import tflite_runtime.interpreter as tflite
+import platform
 
-
-EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
+EDGETPU_SHARED_LIB = {
+  'Linux': 'libedgetpu.so.1',
+  'Darwin': 'libedgetpu.1.dylib',
+  'Windows': 'edgetpu.dll'
+}[platform.system()]
 
 
 def load_labels(path, encoding='utf-8'):
--- python/examples/detection/detect_image.py
+++ python/examples/detection/detect_image.py
@@ -22,9 +22,13 @@ from PIL import ImageDraw
 
 import detect
 import tflite_runtime.interpreter as tflite
+import platform
 
-
-EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
+EDGETPU_SHARED_LIB = {
+  'Linux': 'libedgetpu.so.1',
+  'Darwin': 'libedgetpu.1.dylib',
+  'Windows': 'edgetpu.dll'
+}[platform.system()]
 
 
 def load_labels(path, encoding='utf-8'):
